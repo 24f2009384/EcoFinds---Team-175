6a415a4ae138919f96e118ea65073bf759e1e370
Shreya Saxena <shreya.saxena.ogs@gmail.com>
2025-05-18 14:56:28 +0530

    db changes

--- backend/models.py
+++ backend/models.py
@@ -31,7 +31,7 @@ class Product(db.Model):
     date_added = db.Column(db.DateTime, nullable=False)
     
     # relationships
-    user = db.relationship('User', backref=db.backref('products', lazy=True))
+    user = db.relationship('User', backref=db.backref('products', lazy=True), cascade="all, delete-orphan")
     
     def __repr__(self):
         return f'<Product {self.name}>'
@@ -42,8 +42,8 @@ class PreviousPurchase(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     
-    user = db.relationship('User', backref=db.backref('previous_purchases', lazy=True))
-    product = db.relationship('Product', backref=db.backref('previous_purchases', lazy=True))
+    user = db.relationship('User', backref=db.backref('previous_purchases', lazy=True), cascade="all, delete-orphan")
+    product = db.relationship('Product', backref=db.backref('previous_purchases', lazy=True), cascade="all, delete-orphan")
     
     def __repr__(self):
         return f'<PreviousPurchase {self.user.username} - {self.product.name}>'
@@ -54,8 +54,9 @@ class Cart(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     
-    user = db.relationship('User', backref=db.backref('carts', lazy=True))
-    product = db.relationship('Product', backref=db.backref('carts', lazy=True))
+    user = db.relationship('User', backref=db.backref('carts', lazy=True), cascade="all, delete-orphan")
+    #cascade delete from cart to product
+    product = db.relationship('Product', backref=db.backref('carts', lazy=True), cascade="all, delete-orphan")
     
     def __repr__(self):
         return f'<Cart {self.user.username} - {self.product.name}>'
